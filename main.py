e9c0762309965af392553b541ea5334f0ce2891a
letohell <jury.cromvel@gmail.com>
2026-03-30 11:44:26 +0300

    update comments

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ def get_highest_hero(gender: str, has_job: bool):
     max_height = 0
 
     for h in heroes:
+        #пол
         if h["appearance"]["gender"] != gender:
             continue
 
+        #работа
         occupation = h["work"]["occupation"]
         has_occupation = occupation not in ["-", ""]
 
         if has_job != has_occupation:
             continue
-
+        
+        #рост
         height_str = h["appearance"]["height"][1]
 
         if height_str == "0 cm":
@@ -24,8 +27,9 @@ def get_highest_hero(gender: str, has_job: bool):
 
         height = int(float(height_str.split()[0]))
 
+        #максимальный рост
         if height > max_height:
             max_height = height
             highest = h
-
+    #имя найденого героя или none
     return highest["name"] if highest else None
